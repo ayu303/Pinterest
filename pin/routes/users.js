b37db1f2baa24e21c48060fbfc9dadcd0a1f7f15
ayu303 <ayu.sharma.303@gmail.com>
2024-06-10 15:30:09 +0530

    updating..env file setup

--- pin/routes/users.js
+++ pin/routes/users.js
@@ -1,6 +1,8 @@
 const mongoose=require('mongoose');
 const plm=require("passport-local-mongoose");
-mongoose.connect("mongodb://127.0.0.1:27017/pin");
+require('dotenv').config(); // Load environment variables from .env file
+
+mongoose.connect(process.env.MONGODB_URI);
 
 const userSchema = mongoose.Schema(
   {
