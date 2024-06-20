da482a8102a6cd7291d765298738460c56014cd4
ayu303 <ayu.sharma.303@gmail.com>
2024-06-20 21:41:43 +0530

    multer changes

--- pin/routes/multer.js
+++ pin/routes/multer.js
@@ -1,20 +1,17 @@
 const multer  = require('multer');
 const { v4: uuidv4 } = require('uuid');
 const path = require('path');
-const fs = require('fs');
-const uploadDir = './public/images/uploads';
-if (!fs.existsSync(uploadDir)){
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
+
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, uploadDir);
-  },
-  filename: function (req, file, cb) {
-    const unique = uuidv4();
-    cb(null, unique + path.extname(file.originalname));
-  }
-});
+    destination: function (req, file, cb) {
+      cb(null, './public/images/uploads')
+    },
+    filename: function (req, file, cb) {
+      const unique = uuidv4();
+      
+      cb(null, unique + path.extname(file.originalname));
+    }
+  })
   
   const upload = multer({ storage: storage })
   module.exports=upload;
