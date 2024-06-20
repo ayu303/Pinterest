d57bf9ed564a4561b8811f0deed6094c12892799
ayu303 <ayu.sharma.303@gmail.com>
2024-06-20 22:09:58 +0530

    multer storage cloudinary

--- pin/routes/multer.js
+++ pin/routes/multer.js
@@ -1,22 +1,33 @@
+// const multer  = require('multer');
+// const { v4: uuidv4 } = require('uuid');
+// const path = require('path');
+// const storage = multer.diskStorage({
+//     destination: function (req, file, cb) {
+//       cb(null, './public/images/uploads')
+//     },
+//     filename: function (req, file, cb) {
+//       const unique = uuidv4();
+      
+//       cb(null, unique + path.extname(file.originalname));
+//     }
+//   })
+  
+//   const upload = multer({ storage: storage })
+//   module.exports=upload;
+
 const multer = require('multer');
-const { v4: uuidv4 } = require('uuid');
-const path = require('path');
-const fs = require('fs');
+const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('./cloudinary');
 
-const uploadDir = './public/images/uploads';
-if (!fs.existsSync(uploadDir)) {
-    fs.mkdirSync(uploadDir, { recursive: true });
-}
-
-const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, uploadDir);
-    },
-    filename: function (req, file, cb) {
-      const unique = uuidv4();
-      cb(null, unique + path.extname(file.originalname));
-    }
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: 'uploads',
+    format: async (req, file) => 'jpg', // supports promises as well
+    public_id: (req, file) => file.filename,
+  },
 });
 
 const upload = multer({ storage: storage });
+
 module.exports = upload;
